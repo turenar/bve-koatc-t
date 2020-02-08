a0819f0c94358be8611092423315f5054b5ed4a8
Turenar <siroha@turenar.xyz>
2020-02-08 14:19:58 +0900

    make deceleration 2.5

--- src/koatc/pattern/pattern_const.hxx
+++ src/koatc/pattern/pattern_const.hxx
@@ -7,6 +7,6 @@ constexpr double minimum_location = std::numeric_limits<int>::min();
 constexpr double maximum_location = std::numeric_limits<int>::max();
 
 constexpr double safety_pattern_offset = 15;
-constexpr double default_deceleration = 2.;
+constexpr double default_deceleration = 2.5;
 constexpr int no_pattern = 999;
 } // namespace turenar::koatc::pattern
